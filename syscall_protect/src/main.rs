2cfcacc44f66e7fbdf70c08e374561336c7936e3
Technohacker <technohacker1995@gmail.com>
2023-11-21 03:20:18 +0530

    Add support for policy file reading and syscall control

---
+++ syscall_protect/src/main.rs
@@ -0,0 +1,32 @@
+use std::{env, error::Error, fs, process};
+
+fn try_or_error<T, E: Error>(res: Result<T, E>, msg: &'static str) -> Result<T, String> {
+    res.map_err(|x| format!("{msg}: {x}"))
+}
+
+fn main() -> Result<(), String> {
+    let mut args = env::args();
+
+    let prog_name = args.next().expect("Missing program name");
+
+    if args.len() < 2 {
+        eprintln!("Usage: {prog_name} <policy file> <program> [args...]");
+        process::exit(1);
+    }
+
+    let policy_path = args.next().expect("Missing policy path?");
+    let policy = try_or_error(
+        fs::read_to_string(policy_path),
+        "Failed to read policy file",
+    )?;
+
+    try_or_error(
+        fs::write("/proc/syscall_protect/start", policy),
+        "Failed to open syscall_protect start, is the module loaded?",
+    )?;
+
+    let mut args = args.peekable();
+    let err = exec::execvp(args.peek().expect("No program?").clone(), args);
+
+    panic!("Failed to swap program! {err}");
+}
--- syscall_protect_kmod/syscall_protect.rs
+++ syscall_protect_kmod/syscall_protect.rs
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0
+//! Kernel-level syscall policy enforcer
 
-//! Rust out-of-tree sample
+use kernel::{
+    prelude::*,
+    procfs::{ProcDirectory, ProcFile},
+    task::Task,
+};
 
-use kernel::{prelude::*, types::Opaque};
+mod syscall_names;
 
 module! {
     type: SyscallProtect,
@@ -13,7 +18,9 @@ module! {
 }
 
 struct SyscallProtect {
-    procfs_root: Opaque<*mut kernel::bindings::proc_dir_entry>,
+    _procfs_root: ProcDirectory,
+
+    _start_entry: Pin<Box<ProcFile>>,
 }
 
 unsafe impl Send for SyscallProtect {}
@@ -23,24 +30,39 @@ impl kernel::Module for SyscallProtect {
     fn init(_module: &'static ThisModule) -> Result<Self> {
         pr_info!("syscall_protect starting up...\n");
 
-        unsafe {
-            Ok(SyscallProtect {
-                procfs_root: Opaque::new(kernel::bindings::proc_create(
-                    kernel::c_str!("syscall_protect").as_char_ptr(),
-                    0644,
-                    core::ptr::null_mut(),
-                    core::ptr::null(),
-                ))
-            })
-        }
+        let procfs_root = ProcDirectory::new(kernel::c_str!("syscall_protect"), None);
+        let start_entry = ProcFile::new(kernel::c_str!("start"), Some(&procfs_root))
+            .with_write(Self::start_write);
+
+        Ok(SyscallProtect {
+            _procfs_root: procfs_root,
+
+            _start_entry: start_entry,
+        })
+    }
+}
+
+impl SyscallProtect {
+    fn start_write(_buf: &[u8]) -> Option<Error> {
+        let current: &Task = kernel::current!();
+
+        pr_info!("Enforcement requested for PID: {}", current.pid());
+        // Policy file loaded here
+
+        current.register_seccomp_callback((), Self::scg_step);
+
+        None
+    }
+
+    fn scg_step(_ctx: &mut (), syscall_num: i32) -> bool {
+        syscall_names::print_syscall(syscall_num as u32);
+
+        true
     }
 }
 
 impl Drop for SyscallProtect {
     fn drop(&mut self) {
         pr_info!("syscall_protect shutting down...\n");
-        unsafe {
-            kernel::bindings::proc_remove(*self.procfs_root.get());
-        }
     }
 }
